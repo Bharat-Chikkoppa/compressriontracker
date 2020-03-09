b6a8e1b23d616ff1d1b4e869fef4708bb12b7298
Bharat Chikkoppa <bharat.mc95@gmail.com>
2020-03-09 14:10:00 +0000

    commit 1

--- screens/Prescription.js
+++ screens/Prescription.js
@@ -5,30 +5,60 @@ import Slider from "react-native-slider";
 import { Divider, Button, Block, Text, Switch } from "../components";
 import { theme, mocks } from "../constants";
 
-export default class Prescription extends Component{
+class Prescription extends Component {
+  state = {
 
-    state ={
-        details: {}
-    };
+    details: {}
+  };
 
-    componentDidMount() {
-        this.setState({ details: this.props.details });
-        console.log(details);
+  componentDidMount() {
+    this.setState({ details: this.props.details });
+  }
 
-    }
-    
-
-    render(){
+  /*handleEdit(name, text) {
     const { details } = this.state;
-    return(
-    <Block>
-    <Block flex={false} row center space="between" style={styles.header}>
-        <Text h1 bold>
-        Prescription
-        </Text>
-    </Block>
-    <ScrollView showsVerticalScrollIndicator={false}>
-          <Block style={styles.inputs}>
+    details[name] = text;
+
+    this.setState({ details });
+  }
+
+  toggleEdit(name) {
+    const { editing } = this.state;
+    this.setState({ editing: !editing ? name : null });
+  }*/
+
+  //renderEdit(name) {
+    /*const { details, editing } = this.state;
+
+    if (editing === name) {
+      return (
+        <TextInput
+          defaultValue={details[name]}
+          onChangeText={text => this.handleEdit([name], text)}
+        />
+      );
+    }*/
+
+    //return <Text bold>{details[name]}</Text>;
+ // }
+
+  render() {
+    const { details, editing } = this.state;
+
+    return (
+      <Block>
+        <Block flex={false} row center space="between" style={styles.header}>
+          <Text h1 bold>
+            Prescription
+          </Text>
+          <Button>
+            <Image source={details.avatar} style={styles.avatar} />
+          </Button>
+        </Block>
+
+        <ScrollView showsVerticalScrollIndicator={false}>
+        <Divider margin={[theme.sizes.base, theme.sizes.base * 2]} />
+        <Block style={styles.inputs}>
             <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
               <Block>
                 <Text gray2 style={{ marginBottom: 10 }}>
@@ -42,8 +72,8 @@ export default class Prescription extends Component{
                 <Text gray2 style={{ marginBottom: 10 }}>
                   Nurse
                 </Text>
-              </Block>
-              <Text bold>{details.nursename}</Text>
+                <Text bold>{details.nursename}</Text>
+              </Block>             
             </Block>
             <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
               <Block>
@@ -54,100 +84,60 @@ export default class Prescription extends Component{
               </Block>
             </Block>
           </Block>
+         
 
           <Divider margin={[theme.sizes.base, theme.sizes.base * 2]} />
-
           <Block style={styles.inputs}>
-            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
+          <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
               <Block>
                 <Text gray2 style={{ marginBottom: 10 }}>
-                  Doctor
+                  Compression Stockings
                 </Text>
-                <Text bold>{details.doctorname}</Text>
+                <Text bold>{details.stockings}</Text>
               </Block>             
             </Block>
             <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
               <Block>
                 <Text gray2 style={{ marginBottom: 10 }}>
-                  Nurse
+                  Additional Notes
                 </Text>
-              </Block>
-              <Text bold>{details.nursename}</Text>
+                <Text bold>{details.notes}</Text>
+              </Block>             
             </Block>
-            <Block row space="between" margin={[10, 0]} style={styles.inputRow}>
-              <Block>
-                <Text gray2 style={{ marginBottom: 10 }}>
-                  Hospital
-                </Text>
-                <Text bold>{details.hospital}</Text>
-              </Block>
             </Block>
-          </Block>
 
           <Divider />
 
-          <Block style={styles.toggles}>
-            <Block
-              row
-              center
-              space="between"
-              style={{ marginBottom: theme.sizes.base * 2 }}
-            >
-              <Text gray2>Notifications</Text>
-              <Switch
-                value={this.state.notifications}
-                onValueChange={value => this.setState({ notifications: value })}
-              />
-            </Block>
-
-            <Block
-              row
-              center
-              space="between"
-              style={{ marginBottom: theme.sizes.base * 2 }}
-            >
-              <Text gray2>Newsletter</Text>
-              <Switch
-                value={this.state.newsletter}
-                onValueChange={value => this.setState({ newsletter: value })}
-              />
-            </Block>
-          </Block>
+          
         </ScrollView>
-    
-    </Block>
-    )
-    }
-      
-    };
-    const styles = StyleSheet.create({
-      header: {
-        paddingHorizontal: theme.sizes.base * 2
-      },
-      avatar: {
-        height: theme.sizes.base * 2.2,
-        width: theme.sizes.base * 2.2
-      },
-      inputs: {
-        marginTop: theme.sizes.base * 0.7,
-        paddingHorizontal: theme.sizes.base * 2
-      },
-      inputRow: {
-        alignItems: "flex-end"
-      },
-      sliders: {
-        marginTop: theme.sizes.base * 0.7,
-        paddingHorizontal: theme.sizes.base * 2
-      },
-      thumb: {
-        width: theme.sizes.base,
-        height: theme.sizes.base,
-        borderRadius: theme.sizes.base,
-        borderColor: "white",
-        borderWidth: 3,
-        backgroundColor: theme.colors.secondary
-      },
-      toggles: {
-        paddingHorizontal: theme.sizes.base * 2
-      }
-    });
+      </Block>
+    );
+  }
+}
+
+Prescription.defaultProps = {
+  details: mocks.details
+};
+
+export default Prescription;
+
+const styles = StyleSheet.create({
+  header: {
+    paddingHorizontal: theme.sizes.base * 2
+  },
+  avatar: {
+    height: theme.sizes.base * 2.2,
+    width: theme.sizes.base * 2.2
+  },
+  inputs: {
+    marginTop: theme.sizes.base * 0.7,
+    paddingHorizontal: theme.sizes.base * 2
+  },
+  inputRow: {
+    alignItems: "flex-end"
+  },
+  sliders: {
+    marginTop: theme.sizes.base * 0.7,
+    paddingHorizontal: theme.sizes.base * 2
+  }
+});
